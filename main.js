db91aa3927d20422c0faae486f7d3db212223f10
nomadlife <aaeacd@gmail.com>
2018-09-28 12:28:20 +0900

    basic status

---
+++ main.js
@@ -0,0 +1,62 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
+var flash = require('connect-flash');
+var db = require('./lib/db')
+var helmet = require('helmet');
+app.use(helmet())
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(compression());
+app.use(session({
+  //secure: true, //for https connection
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store:new FileStore()
+}))
+app.use(flash());
+
+app.get('/flash',function(req,res){
+  req.flash('info', 'Flash is back')
+  res.send('flash');
+})
+
+app.get('/flash-display', function(req,res){
+  var fmsg = req.flash();
+  console.log(fmsg);
+  res.send(fmsg);
+  // res.render('index',{messages:req.flash('info')})
+})
+
+var passport = require('./lib/passport')(app)
+
+app.get('*',function(request, response, next){
+  request.list = db.get('topics').value();
+    next()
+})
+
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
+var authRouter = require('./routes/auth')(passport);
+app.use('/',indexRouter);
+app.use('/topic', topicRouter);
+app.use('/auth', authRouter);
+
+
+
+app.use(function(req, res, next){
+  res.status(404).send('sorry cant find that!')
+})
+
+app.use(function(err,req,res,nex){
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+})
---
+++ routes/auth.js
@@ -0,0 +1,101 @@
+var express = require('express')
+var router = express.Router()
+var template = require('../lib/template')
+var auth = require('../lib/auth')
+var shortid = require('shortid');
+var db = require('../lib/db')
+var bcrypt = require('bcryptjs');
+
+module.exports = function (passport) {
+  router.get('/login', function (request, response) {
+    var fmsg = request.flash();
+    var feedback = '';
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+
+    var title = 'WEB - login';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+  <div style="color:red;">${feedback}</div>
+  <form action="/auth/login_process" method="post">
+  <p><input type="text" name="email" placeholder="email" value="test@gmail.com"></p>
+  <p><input type="password" name="pwd" placeholder="password" value="111111"></p>
+  <p>
+  <input type="submit" value="login">
+  </p>
+  </form>
+  `, '' ,auth.statusUI(request, response));
+    response.send(html);
+  })
+
+
+router.post('/login_process',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureFlash: true,
+    successFlash: true
+  }));
+
+router.get('/register', function (request, response) {
+  var fmsg = request.flash();
+  var feedback = '';
+  if (fmsg.error) {
+    feedback = fmsg.error[0];
+  }
+
+  var title = 'user register';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+<div style="color:red;">${feedback}</div>
+<form action="/auth/register_process" method="post">
+<p><input type="text" name="email" placeholder="email" value="test2@gmail.com"></p>
+<p><input type="password" name="pwd" placeholder="password" value="111111"></p>
+<p><input type="password" name="pwd2" placeholder="password" value="111111"></p>
+<p><input type="text" name="displayName" placeholder="display name" value="tester2"></p>
+<p><input type="submit" value="register"></p>
+</form>
+`, '');
+  response.send(html);
+})
+
+router.post('/register_process', function (request, response) {
+  // todo : validation
+  // check email duplicaation check
+  // check if pwd,pwd2 are same
+  var post = request.body;
+  var email = post.email;
+  var pwd = post.pwd;
+  var pwd2 = post.pwd2;
+  var displayName = post.displayName;
+  if(pwd !== pwd2){
+    request.flash('error','password must same!');
+    response.redirect('/auth/register');
+  }else{
+    bcrypt.hash(pwd, 10, function(err, hash) {
+      console.log('hash',hash);
+      var user = {
+        id:shortid.generate(),
+        email:email,
+        password:hash,
+        displayName:displayName
+      }
+        db.get('users').push(user).write();
+        request.login(user, function(err){
+          console.log('redirect');
+          return response.redirect('/');
+        })
+  });
+
+
+    }
+});
+
+router.get('/logout', function (request, response) {
+  request.logout();
+  response.redirect('/');
+})
+
+  return router;
+}
